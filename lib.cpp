f990384c8fe405af71b1905ca290f0193e3325e6
otus-learn-ivan <belyayvan@yandex.ru>
2024-05-19 22:21:26 +0600

    test started

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
-// #include "version.h"
+#include "version.h"
 
-// int version() {
-//     return PROJECT_VERSION_PATCH;
-// }
+int version() {
+    return PROJECT_VERSION_PATCH;
+}
