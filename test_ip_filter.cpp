fd830fc5aede80cd964b767ad2ef5a8be023c445
otus-learn-ivan <belyayvan@yandex.ru>
2024-05-22 21:45:41 +0600

    --target test  working lokal

--- test_ip_filter.cpp
+++ test_ip_filter.cpp
@@ -26,30 +26,31 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
   std::cout <<"test_valid_version" << std::endl;
   BOOST_CHECK(version() != 100);
 }
-//  BOOST_AUTO_TEST_CASE(ip_filter_md5sum)
-//  {
-//     system("cat ip_filter.tsv | ./ip_filter | md5sum > 1.txt");
+BOOST_AUTO_TEST_CASE(ip_filter_md5sum)
+{
+  std::cout << "START ip_filter_md5sum" <<std::endl;
+  system("cat ip_filter.tsv | ./ip_filter | md5sum > 1.txt");
 
-//     std::string line_test;
-//     std::ifstream in("1.txt"); 
-//     if (in.is_open())
-//     {
-//         std::string line;
-//         while (std::getline(in, line))
-//         {
-//             std::istringstream iss{line};
-//             iss >> std::ws >> line_test;
-//         }
-//     }else{
-//       std::cout<<"file not open" << "\n";
-//     }
-    
-//     in.close(); 
+  std::string line_test;
+  std::ifstream in("1.txt"); 
+  if (in.is_open())
+  {
+      std::string line;
+      while (std::getline(in, line))
+      {
+          std::istringstream iss{line};
+          iss >> std::ws >> line_test;
+      }
+  }else{
+    std::cout<<"file not open" << "\n";
+  }
+  
+  in.close(); 
 
-//     std::string true_line = "24e7a7b2270daee89c64d3ca5fb3da1a";
+  std::string true_line = "24e7a7b2270daee89c64d3ca5fb3da1a";
 
-//     BOOST_CHECK(line_test == true_line);
-//     system("rm 1.txt");
-// }
+  BOOST_CHECK(line_test == true_line);
+  system("rm 1.txt");
+}
 
 BOOST_AUTO_TEST_SUITE_END()
